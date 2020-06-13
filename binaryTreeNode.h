b13fc88612f97593c5cd217a23dc4887d6db404f
lvqang <lvqiang1208@163.com>
2020-06-13 20:59:06 +0800

    learning

--- binaryTreeNode.h
+++ binaryTreeNode.h
@@ -4,11 +4,20 @@
 template<typename T> class binaryTreeNode//创建树节点
 {
 public:
-        binaryTreeNode();
-        binaryTreeNode();
+        binaryTreeNode()
+        {
+                leftChild = rightChild = NULL;
+        }
+        binaryTreeNode(const T &theElement, binaryTreeNode<T> *theLeftChild = NULL, binaryTreeNode<T> *theRightChild = NULL)
+        {
+                element(theElement);//无法确定T 的类型，所以使用初始化方式
+                leftChild = theLeftChild;
+                rightChild = theRightChild;
+        }
 protected:
 
 private:
         T element;
+        binaryTreeNode<T> *leftChild, *rightChild;
 
 }
